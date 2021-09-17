cea42aa538b4274f61abd79654959e1a472c87fc
宋扬 <475511896@qq.com>
2021-09-17 21:39:32 +0800

    css finish

--- js/index.js
+++ js/index.js
@@ -10,4 +10,10 @@ $(function(){
         $(this).attr("src",real_img)
         $(this).attr("real_img",img_src)
     })
+
+
+    $(".part_3_nav ul li").mouseenter(function (){
+        $(this).addClass("part_3_nav_active").siblings().removeClass('part_3_nav_active')
+        $(".part_3_content_left_top").eq($(this).index()).stop().fadeIn().siblings().stop().hide()
+    })
 })

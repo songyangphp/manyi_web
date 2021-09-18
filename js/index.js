94a981f711177682dd5695608a19af7c5b35b646
宋扬 <475511896@qq.com>
2021-09-18 11:54:09 +0800

    songyang commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(function(){
+    /*合作伙伴 鼠标划过效果*/
     $(".part_6_content_img img").mouseenter(function (){
         var real_img = $(this).attr("real_img");
         var img_src = $(this).attr("src");
@@ -12,11 +13,13 @@ $(function(){
     })
 
 
+    /*主要项目 切换效果*/
     $(".part_3_nav ul li").mouseenter(function (){
         $(this).addClass("part_3_nav_active").siblings().removeClass('part_3_nav_active')
-        $(".part_3_content_left_top").eq($(this).index()).fadeIn().siblings().hide()
+        $(".part_3_content_left_top").eq($(this).index()).stop().fadeIn().siblings().hide()
     })
 
+    /*吸顶条效果*/
     $(document).scroll(function (){
         var header = $(".header")
         var header_height = header.height()
@@ -27,6 +30,7 @@ $(function(){
         }
     })
 
+    /*返回顶部效果*/
     $(".dingbu").click(function (){
         dingbu_timer = setInterval(function (){
             if($(document).scrollTop() <= 0){
